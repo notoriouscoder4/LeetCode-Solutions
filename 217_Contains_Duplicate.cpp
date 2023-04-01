c5ff6d3a4bb2a1eb70b142a8d01ab67440bc079d
Sawan Kumar <notoriouscoder4@gmail.com>
2023-04-01 14:59:48 +0530

    LeetCode Solutions in C++

--- 217_Contains_Duplicate.cpp
+++ 217_Contains_Duplicate.cpp
@@ -1,5 +1,6 @@
 /*
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
 
 Example 1:
 Input: nums = [1,2,3,1]
@@ -21,21 +22,66 @@ Constraints:
 #include <bits/stdc++.h>
 using namespace std;
 
-bool containsDuplicate(vector<int> &nums)
+class Solution
 {
-      sort(nums.begin(), nums.end());
-      for (int i = 1; i < nums.size(); ++i)
+public:
+      /*
+            TC: O(NlogN)
+            SC: O(1)
+      */
+      bool containsDuplicate_sorting(vector<int> &nums)
       {
-            if (nums[i] == nums[i - 1])
-                  return true;
+            sort(nums.begin(), nums.end());
+            for (int i = 1; i < nums.size(); ++i)
+            {
+                  if (nums[i] == nums[i - 1])
+                        return true;
+            }
+            return false;
       }
-      return false;
-}
+
+      /*
+            TC: O(N)
+            SC: O(N)
+      */
+      bool containsDuplicate_hashset(vector<int> &nums)
+      {
+            unordered_set<int> s;
+
+            for (int i = 0; i < nums.size(); ++i)
+            {
+                  if (s.find(nums[i]) != s.end())
+                        return true;
+                  s.insert(nums[i]);
+            }
+            return false;
+      }
+
+      /*
+            TC: O(N)
+            SC: O(N)
+      */
+      bool containsDuplicate_hashmap(vector<int> &nums)
+      {
+            unordered_map<int, int> mp;
+
+            for (int i = 0; i < nums.size(); ++i)
+            {
+                  if (mp.find(nums[i]) != mp.end())
+                        return true;
+                  mp[nums[i]]++;
+            }
+            return false;
+      }
+};
 
 int main()
 {
       vector<int> nums{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-      bool containDuplicate = containsDuplicate(nums);
+
+      Solution obj;
+      auto containDuplicate = obj.containsDuplicate_hashmap(nums);
       cout << containDuplicate << "\n";
+
       return 0;
 }
