0168184252acf7b7fa16aa5df7d4e53ae2ea2e68
Sawan Kumar <notoriouscoder4@gmail.com>
2023-04-04 02:43:14 +0530

    733. Flood Fill

---
+++ 733_Flood_Fill.cpp
@@ -0,0 +1,84 @@
+/*
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel,
+plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+plus any pixels connected 4-directionally to those pixels (also with the same color), and so on.
+Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+
+
+Example 1:
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel),
+all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Example 2:
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+
+Constraints:
+    m == image.length
+    n == image[i].length
+    1 <= m, n <= 50
+    0 <= image[i][j], color < 2^16
+    0 <= sr < m
+    0 <= sc < n
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int getPixel(vector<vector<int>> &image, int x, int y)
+    {
+        return image[x][y];
+    }
+
+    void putPixel(vector<vector<int>> &image, int x, int y, int newColor)
+    {
+        image[x][y] = newColor;
+    }
+
+    void floodFill_backtrack(vector<vector<int>> &image, vector<vector<int>> &res, int x, int y, int newColor, int oldColor)
+    {
+        if (x < 0 || x >= image.size())
+            return;
+        if (y < 0 || y >= image[0].size())
+            return;
+        if (getPixel(image, x, y) != oldColor)
+            return;
+        if (res[x][y] == newColor)
+            return;
+
+        putPixel(res, x, y, newColor);
+
+        /* Recursively check for horizontally & vertically adjacent coordinates */
+        floodFill_backtrack(image, res, x + 1, y, newColor, oldColor);
+        floodFill_backtrack(image, res, x - 1, y, newColor, oldColor);
+        floodFill_backtrack(image, res, x, y + 1, newColor, oldColor);
+        floodFill_backtrack(image, res, x, y - 1, newColor, oldColor);
+
+        /* Recursively check for diagonally adjacent coordinates  */
+        // floodFill_backtrack(image, res, x+1, y-1, newColor, oldColor);
+        // floodFill_backtrack(image, res, x-1, y+1, newColor, oldColor);
+        // floodFill_backtrack(image, res, x+1, y+1, newColor, oldColor);
+        // floodFill_backtrack(image, res, x-1, y-1, newColor, oldColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+    {
+        vector<vector<int>> res = image;
+        int oldColor = image[sr][sc];
+        floodFill_backtrack(image, res, sr, sc, color, oldColor);
+        return res;
+    }
+};
