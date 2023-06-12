dea061f468bb2083e1fb271c5df96771050d49b6
notoriouscoder <notoriouscoder4@gmail.com>
2023-06-12 22:31:27 +0530

    12-06-2023

---
+++ June_23_Daily_Challenge/228_Summary_Ranges.cpp
@@ -0,0 +1,63 @@
+/*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    . "a->b" if a != b
+    . "a" if a == b
+
+
+Example 1:
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+Constraints:
+    0 <= nums.length <= 20
+    -2^31 <= nums[i] <= 2^31 - 1
+    All the values of nums are unique.
+    nums is sorted in ascending order.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> summaryRanges(vector<int> &nums)
+    {
+        vector<string> ranges;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int start = nums[i];
+
+            while (i + 1 < nums.size() && nums[i] + 1 == nums[i + 1])
+                i++;
+
+            if (start != nums[i])
+                ranges.push_back(to_string(start) + "->" + to_string(nums[i]));
+            else
+                ranges.push_back(to_string(start));
+        }
+        return ranges;
+    }
+};
